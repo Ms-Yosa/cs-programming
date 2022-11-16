fdb9be57a6af7dca45e5f3532efcbc3b8571bb13
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-17 01:41:04 +0800

    basics

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿namespace cs_programming;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+     static void Main(string[] args)
+     {
+
+          int myInt = 5;
+          double myDouble = 2.2;
+
+          Console.WriteLine("the value of myInt is {0} and the value of myDouble is {1}", myInt, myDouble);
+
+          // allocate 5 more spaces
+          Console.WriteLine("the value of myInt is {0,5} and the value of myDouble is {1,5}", myInt, myDouble);
+
+          //concat
+          Console.WriteLine("The value of myInt is " + myInt + " " + " and the value of myDouble is " + myDouble + " ");
+     }
 }
