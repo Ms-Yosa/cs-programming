23e2448030224bbde2dbeee4a16239ad3e194e11
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-17 02:21:40 +0800

    basics

--- Program.cs
+++ Program.cs
@@ -3,26 +3,48 @@ class Program
 {
      static void Main(string[] args)
      {
+          /*
 
-          int myInt = 5;
-          double myDouble = 2.2;
+            int myInt = 5;
+            double myDouble = 2.2;
 
-          Console.WriteLine("the value of myInt is {0} and the value of myDouble is {1}", myInt, myDouble);
+            Console.WriteLine("the value of myInt is {0} and the value of myDouble is {1}", myInt, myDouble);
 
-          // allocate 5 more spaces
-          Console.WriteLine("the value of myInt is {0,5} and the value of myDouble is {1,5}", myInt, myDouble);
+            // allocate 5 more spaces
+            Console.WriteLine("the value of myInt is {0,5} and the value of myDouble is {1,5}", myInt, myDouble);
 
-          //concat
-          Console.WriteLine("The value of myInt is " + myInt + " " + " and the value of myDouble is " + myDouble + " ");
+            //concat
+            Console.WriteLine("The value of myInt is " + myInt + " " + " and the value of myDouble is " + myDouble + " ");
 
-          // operators
-          int result = 2 + 3 * 4;
-          Console.WriteLine(result);
+            // operators
+            int result = 2 + 3 * 4;
+            Console.WriteLine(result);
 
-          //pre&post
-          int a = 5;
-          int c = a++;
-          Console.WriteLine("" + a + " " + c);
+            //pre&post
+            int a = 5;
+            int c = a++;
+            Console.WriteLine("" + a + " " + c);
+
+            */
+
+          //String
+
+          string name1 = "Jesse";
+          string name2 = "Jesse";
+
+          //comparing strings
+
+          //   bool namesEquals = name1 == name2;
+          //   bool namesEquals = string.Equals(name1, name2);
+          //   int namesEquals = string.Compare(name1, name2);
+          int namesEquals = name1.CompareTo(name2);
+          // 0 = same | negative = var1 < var2 (vice versa for positive)
+          System.Console.WriteLine(namesEquals);
+          Console.WriteLine(name1.Substring(0, 1));
+
+
+          // substring = Substring (startIndex, numberOfChartersTaken)
+
 
      }
 }
